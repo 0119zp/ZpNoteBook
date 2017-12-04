3f77dd472819e2bfb0d14f8d3ccaf81decf2aa11
zpan <zpzibo0119@163.com>
2017-12-04 22:10:30 +0800

    记事本 - 基础框架搭建

---
+++ app/src/main/java/zp/com/zpnotebook/home/MainActivity.java
@@ -0,0 +1,16 @@
+package zp.com.zpnotebook.home;
+
+import zp.com.zpbase.activity.ZpBaseActivity;
+import zp.com.zpnotebook.R;
+
+public class MainActivity extends ZpBaseActivity {
+
+    @Override
+    protected int exInitLayout() {
+        return R.layout.activity_main;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/zp/com/zpnotebook/login/activity/ZpLoginActivity.java
@@ -0,0 +1,18 @@
+package zp.com.zpnotebook.login.activity;
+
+import zp.com.zpbase.activity.ZpBaseActivity;
+import zp.com.zpnotebook.R;
+
+/**
+ * Created by Administrator on 2017/12/4 0004.
+ */
+
+public class ZpLoginActivity extends ZpBaseActivity{
+
+    @Override
+    protected int exInitLayout() {
+        return R.layout.activity_login;
+    }
+
+
+}
