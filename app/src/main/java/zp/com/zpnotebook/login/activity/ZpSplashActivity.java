1aefbf6ecd8fd09424b65648b0985dbd8da8a264
zpan <zpzibo0119@163.com>
2017-12-04 22:44:16 +0800

    记事本 - 基础框架搭建

---
+++ app/src/main/java/zp/com/zpnotebook/login/activity/ZpSplashActivity.java
@@ -0,0 +1,15 @@
+package zp.com.zpnotebook.login.activity;
+
+import zp.com.zpbase.activity.ZpBaseActivity;
+
+/**
+ * Created by Administrator on 2017/12/4 0004.
+ */
+
+public class ZpSplashActivity extends ZpBaseActivity{
+
+    @Override
+    protected int exInitLayout() {
+        return super.exInitLayout();
+    }
+}
