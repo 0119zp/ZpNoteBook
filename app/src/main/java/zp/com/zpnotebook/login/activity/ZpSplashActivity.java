484fdfaaf361728f8ac42a75cde7fd8eb40cca4a
zpan <zpzibo0119@163.com>
2017-12-06 21:06:08 +0800

    记事本 - 欢迎页面倒计时器添加

--- app/src/main/java/zp/com/zpnotebook/login/activity/ZpSplashActivity.java
+++ app/src/main/java/zp/com/zpnotebook/login/activity/ZpSplashActivity.java
@@ -1,6 +1,13 @@
 package zp.com.zpnotebook.login.activity;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import zp.com.zpbase.activity.ZpBaseActivity;
+import zp.com.zpnotebook.R;
 
 /**
  * Created by Administrator on 2017/12/4 0004.
@@ -8,8 +15,58 @@ import zp.com.zpbase.activity.ZpBaseActivity;
 
 public class ZpSplashActivity extends ZpBaseActivity{
 
+    private TextView mSplashSkip;
+    private CountDownTimer mTimer;
+
     @Override
     protected int exInitLayout() {
-        return super.exInitLayout();
+        return R.layout.activity_splash;
     }
+
+    @Override
+    protected void exInitView() {
+        super.exInitView();
+
+        mSplashSkip = findViewById(R.id.tv_spalsh_timer);
+        mSplashSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZpSplashActivity.this, ZpLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        initTimer();
+    }
+
+    private void initTimer() {
+        mTimer = new CountDownTimer((long) (5 * 1000), 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int remainTime = (int) (millisUntilFinished / 1000L);
+                mSplashSkip.setText(getResources().getString(R.string.zp_splash_skip, new Object[] {Integer.valueOf(remainTime)}));
+            }
+
+            @Override
+            public void onFinish() {
+                mSplashSkip.setText(getResources().getString(R.string.zp_splash_skip, new Object[] {Integer.valueOf(0)}));
+                Intent intent = new Intent(ZpSplashActivity.this, ZpLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        mTimer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mTimer != null) {
+            mTimer.cancel();
+            mTimer = null;
+        }
+    }
+
 }
